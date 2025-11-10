8c1d04dfbbf0dac86dd38dc9169a9c40ec4eb4a3
Illia Dobrovolskyi <illia.dobrovolskyi2@gmail.com>
2025-11-10 22:02:15 +0200

    Implemented all methods and custom predicate

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,23 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final String NATIONALITY = "Ukrainian";
+    private static final int AGE = 35;
+    private static final int REQ_YEARS_IN_UKRAINE = 10;
+    private static final int ONE = 1;
+    private static final int ZERO = 0;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] splitted = candidate.getPeriodsInUkr().split("-");
+        int periodsInUkraine = Integer.parseInt(splitted[ONE])
+                - Integer.parseInt(splitted[ZERO]);
+        return candidate.getNationality().equals(NATIONALITY)
+                && candidate.getAge() >= AGE
+                && periodsInUkraine >= REQ_YEARS_IN_UKRAINE
+                && candidate.isAllowedToVote();
+    }
 }
